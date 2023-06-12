2f044b307ec03d7161f292bc887d057bc76ab80a
morris46202 <98511302+morris46202@users.noreply.github.com>
2023-06-12 10:09:34 +0000

    修改search.php
    -search.php
        -背景設定完成
        -nav bar新增回首頁

--- page/search.php
+++ page/search.php
@@ -5,68 +5,82 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../style/search.css">
 </head>
 
 <body>
-    <h1>This is search page</h1>
+  <div class="box">
 
-    <form action="dosearch.php" method="post">	
-        <table width="700" border="1" bgcolor="#cccccc" align="center">
-            <tr>
-                <th>search by coursename</th>
-                <td bgcolor="grey"><input type="text" name="search_course"  /></td>
-            </tr>
-            <tr>
-                <th>search by instructor name</th>
-                <td bgcolor="grey"><input type="text" name="search_name"  /></td>
-            </tr>
-            
-            <tr>
-            <th><label for="search_dept">Select Department:</label></th>
-                <td bgcolor="grey">
+    <div class="background">
+      <!-- this div is for background -->
+    </div>
+
+    <div class="this_class_is_for_nav_bar">
+      <nav class="navbar">
+          <a href="page/signup.html" target="_self" class="signup_pos"> SIGN UP</a>
+          <a href="page/login.html" target="_self" class="login_pos"> LOG IN</a>
+          <a href="../index.html" target="_self" class="return_indexpage_pos">回到首頁</a>
+      </nav>
+    </div>
+
+    <div class="main_display_box">
+      <form action="dosearch.php" method="post">	
+        <table class="table_box">
+              <tr >
+                  <th>search by coursename</th>
+                  <td><input type="text" name="search_course"  class="boarder_test" /></td>
+              </tr>
+              <tr>
+                  <th>search by instructor name</th>
+                  <td bgcolor="grey"><input type="text" name="search_name"  /></td>
+              </tr>
+              
+              <tr>
+                <th><label for="search_dept">Select Department:</label></th>
+                  <td bgcolor="grey">
                     <select name="search_dept"  id = "search_dept">
-                        <option value=""></option>
-                        <?php
-                            //******** update your personal settings ******** 
-                            $servername = "140.122.184.125:3307";
-                            $username = "team14";
-                            $password = "kQVYoJa7S0NIXlCN";
-                            $dbname = "team14";
-
-                            //Connecting to and selecting a MySQL database
-                            $conn = new mysqli($servername, $username, $password, $dbname);
-
-                            if (!$conn->set_charset("utf8")) {
-                                printf("Error loading character set utf8: %s\n", $conn->error);
-                                exit();
-                            }
-
-                            // Check connection
-                            if ($conn->connect_error) {
-                                die("Connection failed: " . $conn->connect_error);
-                            } 
-
-                            $sql = "SELECT dept_name FROM department";
-                            $result = $conn->query($sql);
-
-                            
-                            if ($result->num_rows > 0) {
-                                while ($row = $result->fetch_assoc()) {
-                                    $deptName = $row['dept_name'];                                  
-                                    echo "<option value='$deptName'>$deptName</option>";
-                                }
-                            } else {
-                                echo "<option value=''>No departments found</option>";
-                            }
-                        ?>
-                    </select>
-                </td>
-            </tr>
-            
-            </tr>
-            <th><label for="search_day">Select Day:</label></th>
+                      <option value=""></option>
+                          <?php
+                              //******** update your personal settings ******** 
+                              $servername = "140.122.184.125:3307";
+                              $username = "team14";
+                              $password = "kQVYoJa7S0NIXlCN";
+                              $dbname = "team14";
+  
+                              //Connecting to and selecting a MySQL database
+                              $conn = new mysqli($servername, $username, $password, $dbname);
+  
+                              if (!$conn->set_charset("utf8")) {
+                                  printf("Error loading character set utf8: %s\n", $conn->error);
+                                  exit();
+                              }
+  
+                              // Check connection
+                              if ($conn->connect_error) {
+                                  die("Connection failed: " . $conn->connect_error);
+                              } 
+  
+                              $sql = "SELECT dept_name FROM department";
+                              $result = $conn->query($sql);
+  
+                              
+                              if ($result->num_rows > 0) {
+                                  while ($row = $result->fetch_assoc()) {
+                                      $deptName = $row['dept_name'];                                  
+                                      echo "<option value='$deptName'>$deptName</option>";
+                                  }
+                              } else {
+                                  echo "<option value=''>No departments found</option>";
+                              }
+                          ?>
+                      </select>
+                  </td>
+              </tr>
+              
+              <tr>
+                <th><label for="search_day">Select Day:</label></th>
                 <td bgcolor="grey">
-                    <select name="search_day"  id = "search_day">
+                  <select name="search_day"  id = "search_day">
                         <option value= "" ></option>
                         <option value="一">一</option>
                         <option value="二">二</option>
@@ -75,6 +89,9 @@
                         <option value="五">五</option>
                     </select>  
                 </td>
+              <!-- </tr>
+
+              <tr> -->
                 <th><label for="search_time1">Select time1:</label></th>
                 <td bgcolor="grey">
                     <select name="search_time1" id="search_time1">
@@ -86,6 +103,9 @@
                         ?>
                     </select>
                 </td>
+              <!-- </tr>
+
+              <tr> -->
                 <th><label for="search_time2">Select time2:</label></th>
                 <td bgcolor="grey">
                     <select name="search_time2" id="search_time1">
@@ -97,12 +117,13 @@
                         ?>
                     </select>
                 </td>
-            </tr>
-           
-
+              </tr>
         </table>
-        <input type="submit" value="search">
-    </form>  
-
+          <input type="submit" value="search">
+      </form>  
+    </div>
+  
+  </div>
+  
 </body>
 </html>

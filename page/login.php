d251e76c15420dd40e87e7da48bf24ca2c812806
hsiaooooooo <alllllvin21292@gmail.com>
2023-06-14 09:15:16 +0800

    Update login.php

--- page/login.php
+++ page/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +8,7 @@
     <title>登入</title>
     <link rel="stylesheet" href="../style/login.css">
 </head>
+
 <body>
     <div class="box">
 
@@ -14,7 +16,7 @@
             <!-- hehe -->
         </div>
 
-        <div >
+        <div>
             <nav class="navbar">
                 <!-- <a href="/page/signup.html" target="_self" class="signup_pos"> SIGN UP</a>
                 <a href="/page/login.html" target="_self" class="login_pos"> LOG IN</a>
@@ -22,9 +24,10 @@
                 <p class="login_page_title">登入</p>
             </nav>
         </div>
-    
-        <?php 
-        $sn = isset($_GET['serial_no']) ? $_GET['serial_no'] : 'hi'; 
+
+        <?php
+        session_start();
+        $sn = isset($_GET['serial_no']) ? $_GET['serial_no'] : 'hi';
         echo $sn;
         ?>
 
@@ -34,13 +37,21 @@
                 <br><br><br><br><br>
                 <p>登入您的帳號</p>
                 <br>
-                <p>電子郵件：<input type="email" class="only_underline" name="email" required="required" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/></p>
-                <p>　　密碼：<input type="password" class="only_underline" name="password" required="required"/></p>
+                <p>電子郵件：<input type="email" class="only_underline" name="email" required="required"
+                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" /></p>
+                <p>　　密碼：<input type="password" class="only_underline" name="password" required="required" /></p>
+                <?php
+                if (isset($_SESSION['msg'])) {
+                    echo "<p><font color='#FF0000'>{$_SESSION['msg']}</font></p>";
+                }
+                session_unset();
+                ?>
                 <br>
                 <button class="login_button" type="submit">登入</button>
             </div>
         </form>
-     
+
     </div>
 </body>
+
 </html>

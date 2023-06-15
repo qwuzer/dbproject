249c40f867c1647e5abff46ac972addf17ef96ba
hsiaooooooo <alllllvin21292@gmail.com>
2023-06-15 15:32:33 +0800

    Update doupdate.php

--- page/doupdate.php
+++ page/doupdate.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-if (isset($_POST['course_code']) &&isset($_POST['dept_name']) &&isset($_POST['course_level']) &&isset($_POST['title']) &&isset($_POST['credits']) &&isset($_POST['R/S/G']) &&isset($_POST['full/half']) && isset($_POST['EMI']) && isset($_POST['instructor']) && isset($_POST['time_location'])) {
+if (isset($_POST['course_code']) &&isset($_POST['dept_name']) &&isset($_POST['course_level']) &&isset($_POST['title']) &&isset($_POST['credits']) &&isset($_POST['RSG']) &&isset($_POST['fullhalf']) && isset($_POST['EMI']) && isset($_POST['instructor']) && isset($_POST['time_location'])) {
 	$course_code = $_POST['course_code'];
 	$dept_name = $_POST['dept_name'];
 	$course_level = $_POST['course_level'];
@@ -26,7 +26,7 @@ if (isset($_POST['course_code']) &&isset($_POST['dept_name']) &&isset($_POST['co
 	
 	$id = $_GET['id'];
 	
-	$update_sql = "UPDATE course set course_code='$course_code', dept_name='$dept_name', course_level='$course_level', title='$title', credits='$credits', R/S/G='$R', full/half='$full', EMI='$EMI', instructor='$instructor', time_location='$time_location' where serial_no='$id'"; // TODO
+	$update_sql = "UPDATE course set course_code='$course_code', dept_name='$dept_name', course_level='$course_level', title='$title', credits='$credits', RSG='$R', fullhalf='$full', EMI='$EMI', instructor='$instructor', time_location='$time_location' where serial_no='$id'"; // TODO
 	
 	if ($conn->query($update_sql) === TRUE) {
 		echo "修改成功!!<br> <a href='coursemanage.php'>返回主頁</a>";
